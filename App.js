06219dcf60c494388d42d1e9273656cdc6a9698e
Hershey15620 <67335475+Hershey15620@users.noreply.github.com>
2021-02-25 13:50:15 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import WelcomeScreen from './screens/WelcomeScreen';
+import { AppDrawerNavigator } from './components/AppDrawerNavigator'
+export default function App() {
+  return (
+    <WelcomeScreen/>
+  );
+}
+
+const switchNavigator=createSwitchNavigator({
+  WelcomeScreen:{screen:WelcomeScreen},
+  Drawer:{screen:AppDrawerNavigator}
+})
+const AppContainer= createAppContainer(switchNavigator)
---
+++ CustomSideBarMenu.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import {DrawerItems} from 'react-navigation-drawer';
+import {StyleSheet, View, Text,TouchableOpacity} from 'react-native';
+import firebase from 'firebase';
+
+export default class CustomSideBarMenu extends Component{
+    render(){
+        return(
+<View style={{flex:1}}>
+<View style={styles.drawerItmesContainer}>
+
+    
+    </View>
+
+
+
+
+
+    </View>
+        )
+    }
+}
